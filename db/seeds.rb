b8a040c169cb683b440086cd88c854889670cd97
lina pulgarin <linpulgarin.16@gmail.com>
2022-09-15 19:32:55 -0500

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.find_each(&:destroy)
+
+Restaurant.create(name: "Bigotes", address: "CR 55 # 58A -41", category: "italian")
+Restaurant.create(name: "Juancho", address: "CR 15 # 22b -11", category: "french")
+Restaurant.create(name: "Nutibara", address: "CL 12 # 22A -71", category: "chinese")
+Restaurant.create(name: "Doña Carlota", address: "CR 98 # 22A -01", category: "italian")
+Restaurant.create(name: "Don Pedro", address: "CR 102 # 34 - 88", category: "japanese")
+
+puts 'Inserted register in the database'
